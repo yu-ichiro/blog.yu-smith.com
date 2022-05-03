a11a672c0c3726b6e1c4a6af42d9800b5f77dfd5
Yuichiro Luke Smith <contact@yu-smith.com>
2022-05-04 01:26:04 +0900

    feat: ✨ add GitHub link

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -6,8 +6,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { BlogPostBySlugQuery } from "../../gql"
 import { Tags } from "../components/tags"
-import { ensurePageInfo, Frontmatter, GatsbyPage, LinkProps, PageInfo } from "../../types"
-import { PageInfoComponent } from "../components/pageInfoComponent";
+import {
+  ensurePageInfo,
+  Frontmatter,
+  GatsbyPage,
+  LinkProps,
+  PageInfo,
+} from "../../types"
+import { PageInfoComponent } from "../components/pageInfoComponent"
 
 type PostContext = {
   frontmatter: Frontmatter
@@ -55,29 +61,53 @@ const Post: GatsbyPage<BlogPostBySlugQuery, PostContext> = ({
 
   return (
     <Layout>
-      <Seo title={pageContext.frontmatter.title} link={link} slug={pageContext.frontmatter.slug} />
+      <Seo
+        title={pageContext.frontmatter.title}
+        link={link}
+        slug={pageContext.frontmatter.slug}
+      />
       <header>
         <h1>{pageContext.frontmatter.title}</h1>
         {canonicalLink}
-        <p style={{ margin: 0, paddingBottom: ".5rem" }}>{date.toLocaleDateString()}</p>
-        <Tags tags={pageContext.frontmatter.tags}/>
+        <p style={{ margin: 0, paddingBottom: ".5rem" }}>
+          {date.toLocaleDateString()}&nbsp;
+          <a
+            href={`https://github.com/yu-ichiro/blog.yu-smith.com/tree/master/blog/${pageContext.frontmatter.slug}.md`}
+            target="_blank"
+          >
+            GitHubで開く
+          </a>
+        </p>
+        <Tags tags={pageContext.frontmatter.tags} />
       </header>
       <article
         dangerouslySetInnerHTML={{ __html: data.markdownRemark?.html ?? "" }}
       />
-      <section style={{ display: "flex", justifyContent: "space-between", gap: "3rem", paddingTop: "1rem", borderTop: "solid 1px rgba(87, 76, 241, .15)" }}>
-        { pageContext.next && (
-          <Link to={`/post/${pageContext.next.slug}`}>&lt; {pageContext.next.title}</Link>
-        ) }
-        { pageContext.prev && (
-          <Link to={`/post/${pageContext.prev.slug}`}>{pageContext.prev.title} &gt;</Link>
-        ) }
+      <section
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "3rem",
+          paddingTop: "1rem",
+          borderTop: "solid 1px rgba(87, 76, 241, .15)",
+        }}
+      >
+        {pageContext.next && (
+          <Link to={`/post/${pageContext.next.slug}`}>
+            &lt; {pageContext.next.title}
+          </Link>
+        )}
+        {pageContext.prev && (
+          <Link to={`/post/${pageContext.prev.slug}`}>
+            {pageContext.prev.title} &gt;
+          </Link>
+        )}
       </section>
-      { pageContext.related.length == 0 ? null : (
+      {pageContext.related.length == 0 ? null : (
         <section style={{ zoom: "80%", padding: "2rem 0" }}>
           <h2>関連記事</h2>
           {pageContext.related.map(ensurePageInfo).map(info => (
-            <PageInfoComponent pageInfo={info} key={info.slug}/>
+            <PageInfoComponent pageInfo={info} key={info.slug} />
           ))}
         </section>
       )}
@@ -85,15 +115,15 @@ const Post: GatsbyPage<BlogPostBySlugQuery, PostContext> = ({
   )
 }
 
-export const pageQuery = Post.pageQuery = graphql`
-    query BlogPostBySlug($slug: String!) {
-        site {
-            buildTime
-        }
-        markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-            html
-        }
+export const pageQuery = (Post.pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      buildTime
     }
-`
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+    }
+  }
+`)
 
 export default Post
